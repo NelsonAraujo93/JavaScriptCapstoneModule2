51c1baa546ac2a9d3792ce47944468c8302a5d01
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-02-07 10:37:37 -0600

    Retrieving books data from the api

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.png$/,
+        use: 'file-loader',
+      },
     ],
   },
   plugins: [
