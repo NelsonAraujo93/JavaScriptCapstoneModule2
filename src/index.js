f1e5afb7ecfac0ef89ff954240a7e8981366e908
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-02-07 16:07:49 -0500

    Add pop up and functions to pop up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,83 @@ import Logo from './images/google_books.png';
 
 const bookList = document.getElementById('book-list');
 
+const popUp = (item) => {
+  debugger;
+  const popUpContainer = document.createElement('article');
+  popUpContainer.id = 'pop-up';
+
+  popUpContainer.innerHTML = `
+    <div class="pop-container">
+      <div class="pop-up-header">
+        <h2 class="book-title">${item.volumeInfo.title}</h2>
+        <button id="close-modal-btn">x</button>
+      </div>
+      <div class="pop-up-body">
+        <div class="image-container">
+          <img src='${item.volumeInfo.imageLinks.thumbnail}' class="book-image">
+        </div>
+        <div class="pop-up-content">
+          <div class="left-content">
+            <label>Author:</label>
+            <div class="book-data">${item.volumeInfo.authors[0]}</div>
+            <label>Category:</label>
+            <div class="book-data">${item.volumeInfo.categories[0]}</div>
+          </div>
+          <div class="rigth-content">
+            <label>Published date:</label>
+            <div class="book-data">${item.volumeInfo.publishedDate}</div>
+            <label>Language:</label>
+            <div class="book-data">${item.volumeInfo.language}</div>
+          </div>
+        </div>
+      </div>
+      <div class="pop-up-comments">
+        <h3>Comments (2)</h3>
+        <ul id="comments-list"></ul>
+      </div>
+      <div class="pop-up-form">
+        <h3>Add a comment</h3>
+        <form id="new-comment">
+          <input id="user" type="text" name="user" placeholder="Your name" required></input>
+          <textarea id="comment" name="user" placeholder="Your name" required></textarea>
+          <button class="add-comment" id="comment-btn">Comment</button>
+        </form>
+      </div>
+    </div>
+  `;
+  document.body.append(popUpContainer);
+  const closeBtn = document.getElementById('close-modal-btn');
+  closeBtn.addEventListener('click', () => {
+    popUpContainer.remove();
+  });
+
+  const addComentBtn = document.getElementById('comment-btn');
+  addComentBtn.addEventListener('click', () => {
+    const userInput = document.getElementById('user').value;
+    const textAreaInput = document.getElementById('comment').value;
+
+    debugger;
+  });
+}
+
+const getBookData = async (bookId) => {
+
+}
+
+const createComment = async (bookId, user, comment) => {
+  fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/gcxOnR7Ou6sAxWdfnAQw/comments', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: bookId,
+      username: user,
+      comment: comment,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+}
+
 const bookDetails = async () => {
   const options = ['html', 'css', 'javascript', 'ruby', 'react', 'node', 'jokes', 'java', 'maths', 'art', 'spanish', 'english', 'python', 'sql'];
 
@@ -19,7 +96,7 @@ const bookDetails = async () => {
   // const totalBooks = document.querySelector('.total-books');
   // totalBooks.innerHTML = `"${totalItems}" Books about ${search}`;
 
-  for (let i = 0; i < totalItems; i += 1) {
+  for (let i = 0; i < data.items.length; i += 1) {
     const bookList = document.getElementById('book-list');
     const bookCard = document.createElement('div');
     const contentCard = document.createElement('p');
@@ -32,7 +109,7 @@ const bookDetails = async () => {
                     <br>More details
                 </a>
             </p>
-            <button type="submit" class="add-comment">Comment</button>
+            <button type="submit" class="add-comment" id="comment-btn-${data.items[i].id}">Comment</button>
             &#160;&#160;&#160;&#160;
             <button class="heart">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-heart-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
@@ -42,18 +119,16 @@ const bookDetails = async () => {
             <span class="likes-counter">0</span>
             &#160;&#160;&#160;
             <span class="likes">likes</span>`;
-
     const title1 = `${data.items[i].volumeInfo.title}`;
     title.classList.add('title');
     title.innerHTML = title1;
     bookCard.appendChild(title);
-
     bookCard.classList.add('bookCard');
-
     contentCard.innerHTML = bookCardContent;
-
     bookCard.appendChild(contentCard);
     bookList.appendChild(bookCard);
+    const commentBtn = document.getElementById('comment-btn-'+data.items[i].id);
+    commentBtn.addEventListener('click', () =>{ popUp(data.items[i])});
   }
 };
 
